f5be46143af376fdd99bb7c31af23f52b296e44d
Adedayo Matthew <adedayomatt@gmail.com>
2020-08-09 13:52:28 +0100

    Adjusted subscription route

--- domain/Subscription/route.php
+++ domain/Subscription/route.php
@@ -4,7 +4,7 @@ use Domain\Subscription\Actions\PAYGPayment;
 use Domain\Subscription\Actions\SubscriptionAction;
 use Domain\Subscription\Actions\AccountSubscriptionPageAction;
 
-Route::prefix('@{account:username}')->group(function(){
+Route::prefix('{account:username}')->group(function(){
     Route::get('/subscription', AccountSubscriptionPageAction::class)->name('account.subscription.show');
     Route::post('/subscription', SubscriptionAction::class)->name('subscription.subscribe');
     Route::post('/payg', PAYGPayment::class)->name('subscription.payg');
