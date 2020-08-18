61553bf9581dfa56aebd530ce45ad72cd7598ab1
Adedayo Matthew <adedayomatt@gmail.com>
2020-08-18 16:48:40 +0100

    Homepage fixes

--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -72,7 +72,7 @@
                 <h2>FEATURES</h2>
                 <hr class="bg-custom-primary">
                 <div class="px-3">
-                    <img src="/images/arrow-divider.png" width="100%">
+                    <img src="/images/arrow-divider.PNG" width="100%">
                 </div>
             </div>
             <div class="container">
@@ -96,7 +96,7 @@
 
         <section class="page-section" id="pricing">
             <div class="bg-custom-primary text-center mb-3" style="height: 200px">
-                <img src="/images/arrow-divider2.png" width="100%">
+                <img src="/images/arrow-divider2.PNG" width="100%">
                 <h2 class="text-white">Pricing</h2>
             </div>
             <div class="pricing-container">
@@ -218,17 +218,17 @@
             return {
                 howItWork: [
                     {
-                        icon: 'images/account.png',
+                        icon: 'images/account.PNG',
                         heading: 'Set up your profile',
                         text: 'You can get started with just your email without having to remember any password.'
                     },
                     {
-                        icon: 'images/add-course.png',
+                        icon: 'images/add-course.PNG',
                         heading: 'Add Course',
                         text: 'You get to include the course title, description (Ability to add text, videos and images) and price for each course you desire to add.'
                     },
                     {
-                        icon: 'images/get-paid.png',
+                        icon: 'images/get-paid.PNG',
                         heading: 'Get Paid',
                         text: 'You get paid every time someone registers for your course and you get to add the person to the conferencing or MS of choice'
                     }
@@ -236,47 +236,47 @@
 
                 features: [
                     {
-                        icon: 'images/web.png',
+                        icon: 'images/web.PNG',
                         heading: 'A Customizable Website for your Courses',
                         text: 'Get a beautiful website for less than the cost of a flyer in just a few clicks.'
                     },
                     {
-                        icon: 'images/authorization.png',
+                        icon: 'images/authorization.PNG',
                         heading: 'Authorized Access Only',
                         text: 'Links to your courses will only be made available to people who have successfully paid and enrolled for the course.'
                     },
                     {
-                        icon: 'images/currency.png',
+                        icon: 'images/currency.PNG',
                         heading: 'Multi-Currency',
                         text: 'We do not charge extra for enrollment, your money is all yours.'
                     },
                     {
-                        icon: 'images/zero-fee.png',
+                        icon: 'images/zero-fee.PNG',
                         heading: 'Zero Fees',
                         text: 'We do not charge extra for enrollment, your money is all yours.'
                     },
                     {
-                        icon: 'images/accept-payment.png',
+                        icon: 'images/accept-payment.PNG',
                         heading: 'Accept Payment',
                         text: 'Set a fee for your course, select your  preferred payment method and sleep at night, while we take care of the rest. We integrate with Stripe, PayPal, Paystack and more.'
                     },
                     {
-                        icon: 'images/accept-payment.png',
+                        icon: 'images/accept-payment.PNG',
                         heading: 'Third-Party Integration',
                         text: 'Pick your preferred conferencing or LMS (such as Zoom, Google Hangouts, WhatsApp, Google Classrom, nd others) and submit the URL to your audience.'
                     },
                     {
-                        icon: 'images/participants-list.png',
+                        icon: 'images/participants-list.PNG',
                         heading: 'Participants List',
                         text: 'You get detailed enrollment information of every participant.'
                     },
                     {
-                        icon: 'images/unlimited-enrollment.png',
+                        icon: 'images/unlimited-enrollment.PNG',
                         heading: 'Unlimited Enrollment',
                         text: 'THE MORE THE MERRIER! As many people as enroll for your course. There arre no limit unless they are set by you.'
                     },
                     {
-                        icon: 'images/enterprise-version.png',
+                        icon: 'images/enterprise-version.PNG',
                         heading: 'Enterprise Version',
                         text: 'Allows you to manage several instructors under your organization from a single account.'
                     }
@@ -284,17 +284,17 @@
                 ],
                 communities: [
                     {
-                        icon: 'images/jobred.png',
+                        icon: 'images/jobred.PNG',
                         heading: 'JobRed "Ajo Balogun"',
                         text: 'When the pandemic caused us to stop physical classes, we needed a way to reach out to our students and ensure tjat the quality of out training was not compromised. Acada allowd us to hold classes on Google Classroom while ensuring that only students that paid for such classes had access to them.'
                     },
                     {
-                        icon: 'images/healthboxes.png',
+                        icon: 'images/healthboxes.PNG',
                         heading: 'Health Boxes "Lola Kogbodoku"',
                         text: 'Charging for training and keeping track of participant registrations have been made easier with this app. I do not even need flyers, propspective applicants can simply browse through my page and register for their preferred course.'
                     },
                     {
-                        icon: 'images/prof-elegbe.png',
+                        icon: 'images/prof-elegbe.PNG',
                         heading: 'Professor Elegbe',
                         text: 'I help anesthesiologist prepare for exams with the West African College of Surgeons. Acada has made it easy for me to reach my students during the Covid pandemic, continue with the classes and keep track of them.'
                     }
@@ -411,11 +411,6 @@
         min-height: 300px;
     }
 
-    #features .info-box
-    {
-        min-height: 300px;
-    }
-
     .pricing-container
     {
         margin-top: -100px;
@@ -450,7 +445,6 @@
 
     #community .info-box
     {
-        min-height: 600px;
         background-color: #fff;
     }
 
@@ -460,6 +454,16 @@
     }
 
     @media (min-width: 768px){
+        #features .info-box
+        {
+            min-height: 300px;
+        }
+
+        #community .info-box
+        {
+            min-height: 600px;
+        }
+
         .pricing-container
         {
             padding: 0 100px;
