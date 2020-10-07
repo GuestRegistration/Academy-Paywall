573b72aae78934911c27de9e54ed3a6bef180703
Adedayo Matthew <adedayomatt@gmail.com>
2020-10-07 19:28:57 +0100

    Disallow SE indexing in development environment

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -8,7 +8,11 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
         <meta name="csrf-token" content="{{ csrf_token() }}">
-        <meta name="robots" content="index, follow" />
+        @if (env('APP_ENV') == 'development')
+            <meta name="robots" content="noindex">
+        @elseif(env('APP_ENV') == 'production')
+            <meta name="robots" content="index, follow" />
+        @endif
         <meta name="description" content="{{ $description ?? '' }}">
         <meta name="keywords" content="{{ isset($keywords) ? join(', ', array_merge($keywords, $defaultKeywords)) : join(', ', $defaultKeywords) }}">
         <meta name="environment" content="production">
